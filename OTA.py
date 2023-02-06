832421c7174f9c77f9e23525def441ddc0682412
Adrian Kuboszek <kuboszekadr@gmail.com>
2023-02-06 22:08:45 +0100

    Initial commit

--- OTA.py
+++ OTA.py
@@ -1,4 +1,4 @@
 import requests
 
 files = {'file': open('/git/SmartHome-StairsLight/.pio/build/esp32doit-devkit-v1/firmware.bin', 'rb')}
-requests.post("http://192.168.0.180/update", files=files)
+requests.post("http://192.168.0.103/update", files=files)
